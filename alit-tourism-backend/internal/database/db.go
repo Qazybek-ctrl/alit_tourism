3afb1574c188cfd2895182431c19ac1102f8d19d
Qazybek-ctrl <raimctop@gmail.com>
2025-11-07 20:00:58 +0500

    changed

--- alit-tourism-backend/internal/database/db.go
+++ alit-tourism-backend/internal/database/db.go
@@ -1,31 +1,45 @@
-package db
+package database
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"alit-tourism-backend/internal/models"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-// Musin123!
-// alit-user
-// 5432
 func Connect() *gorm.DB {
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		dsn = "host=127.0.0.1 user=alit-user password=1234 dbname=alit_tourism port=5433 sslmode=disable"
+	// Определяем какой .env файл грузить
+	envFile := "local.env"
+	if os.Getenv("APP_ENV") == "prod" {
+		envFile = "prod.env"
 	}
 
+	// Загружаем переменные окружения из файла
+	err := godotenv.Load(envFile)
+	if err != nil {
+		log.Fatalf("Ошибка загрузки %s: %v", envFile, err)
+	}
+
+	// Получаем DATABASE_URL из env
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL не задана в env файле")
+	}
+
+	// Подключаемся к базе
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Ошибка подключения к базе данных: %v", err)
 	}
 
+	// Миграции
 	if err := db.AutoMigrate(
 		&models.User{},
 		&models.VisaInvitationForm{},
@@ -35,5 +49,6 @@ func Connect() *gorm.DB {
 	}
 
 	DB = db
+	fmt.Printf("✅ Connected to DB (%s)\n", os.Getenv("APP_ENV"))
 	return db
 }
